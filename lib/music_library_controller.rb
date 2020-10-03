30a967679f9c2b7f02e07e612c5002c9603bd26b
Amanda Mongiove <amandamongiove@gmail.com>
2020-10-03 21:27:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -68,7 +68,7 @@ class MusicLibraryController
     def play_song
       puts "Which song number would you like to play?"
       song_number = gets.strip
-      
+
       list_number = Songs.all.length
       if song_number.between(1..list_number)
         Song.all.sort_by {|song| song.name}
