b47bd98d0338f72d3181c40fbb65413d8b88ae55
Amanda Mongiove <amandamongiove@gmail.com>
2020-10-03 19:49:55 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -25,8 +25,9 @@ class MusicLibraryController
   end
 
     def list_songs
+      nunmber = 0
       sorted_songs = Song.all.sort!
-      sorted_songs.each do |song, index|
+      sorted_songs.each do |song|
         puts "number +=1. #{song}"
       end
     end
