c9e528f542a485af29c497e2f994f49e7c8f3e85
Amanda Mongiove <amandamongiove@gmail.com>
2020-10-03 20:42:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -36,7 +36,7 @@ class MusicLibraryController
         puts "#{index + 1}" + ". " + artist.name
       end
     end
-    
+
     def list_genres
       Genre.all.sort_by {|genre| genre.name}.each_with_index do |genre, index|
         puts "#{index + 1}" + ". " + genre.name
