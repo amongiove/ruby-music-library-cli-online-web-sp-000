cd0a78b656518271b9a639affb9c7aa9b792c8e5
Amanda Mongiove <amandamongiove@gmail.com>
2020-10-03 21:10:57 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -47,6 +47,7 @@ class MusicLibraryController
       puts "Please enter the name of an artist:"
       user_artist = gets.strip
       if Artist.all.detect{|artist| artist.name == user_artist}
+        artist = Artist.all.detect{|artist| artist.name == user_artist}
         artist.songs.sort_by {|song| song.name}.uniq.each_with_index do |song, index|
           puts "#{index + 1}" + ". " + song.name
         end
