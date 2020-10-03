18af7caf4d853f5af4134777301f4a322ad65a94
Amanda Mongiove <amandamongiove@gmail.com>
2020-10-03 20:38:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -32,7 +32,7 @@ class MusicLibraryController
     end
 
     def list_artists
-      puts Artist.all
+      Artist.all.each {|artist| puts artist.name}
       Artist.all.sort_by {|artist| artist.name}.each_with_index do |artist, index|
         puts "#{index + 1}" + ". " + artist.name
       end
